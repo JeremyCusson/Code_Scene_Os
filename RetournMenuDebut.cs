00c87f6f93ecd8f35e411d48219a9fb00119cbd9
JeremyCusson <65480663+JeremyCusson@users.noreply.github.com>
2020-05-17 14:49:14 -0400

    Add files via upload

---
+++ RetournMenuDebut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Gère les scènes lors de l'exécusion
+
+public class RetournMenuDebut : MonoBehaviour
+{
+    // Envoie l'utilisateur dans la scène Menu
+    public void RetournerAuMenu()
+    {
+        SceneManager.LoadScene("Menu"); // Charge la scène Menu
+    } 
+}
